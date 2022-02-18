df9aa9032323fe188c67f3428d4673c6014d941c
educavalieri <eduardocavalieri@hotmail.com>
2022-02-18 19:08:59 -0300

    insert service and resource requerements

--- src/main/java/com/devsuperior/movieflix/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.devsuperior.movieflix.repositories;
 
 import com.devsuperior.movieflix.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByEmail(String email);
 }
--- src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -1,22 +1,52 @@
 package com.devsuperior.movieflix.services;
 
+import com.devsuperior.movieflix.dto.UserDTO;
 import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.repositories.UserRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public class UserService implements UserDetailsService {
 
     @Autowired
+    private AuthService authService;
+
+    @Autowired
     private UserRepository userRepository;
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Transactional(readOnly = true)
+    public UserDTO findById(Long id){
+
+        authService.validateSelfOrAdmin(id);
+
+        User entity = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        return new UserDTO(entity);
+
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findUserAuthenticated(){
+
+        User user = authService.Authenticated();
+        User entity = userRepository.findByEmail(user.getUsername());
+        UserDTO dto = new UserDTO(user);
+        return dto;
+
+    }
+
 
     //-----------------------------------------------------------------------------------
     //authentication zone:
